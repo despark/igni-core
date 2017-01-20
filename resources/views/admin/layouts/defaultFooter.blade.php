b48598fe7822f49a3bd48d8d306e70ff3e7d2fea
Ludmil Simeonov <lsimeonov@despark.com>
2017-01-20 14:03:46 +0200

    Add despark default logo in footer.

--- resources/views/admin/layouts/defaultFooter.blade.php
+++ resources/views/admin/layouts/defaultFooter.blade.php
@@ -2,7 +2,7 @@
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
         <a target="_blank" href="http://despark.com" title="Digital design agency and app design London">
-            <img src="{{ asset(config('ignicms.logo')) }}" alt="Despark"/>
+            <img src="{{ asset('images/logo.png') }}" alt="Despark"/>
         </a>
     </div>
     <h4>Despark Website Administration Copyright &reg; {{ date('Y') }}</h4>
