23274b72d826d2c9577bf2a10081359056819d38
Ludmil Simeonov <lsimeonov@despark.com>
2017-01-24 12:15:37 +0200

    Add help to select field template

--- resources/views/admin/formElements/select.blade.php
+++ resources/views/admin/formElements/select.blade.php
@@ -5,6 +5,11 @@ $fieldName = $field->getFieldName();
 <div class="form-group {{ $errors->has($fieldName) ? 'has-error' : '' }}">
     {!! Form::label($fieldName, $field->getLabel()) !!}
     {!! Form::select($fieldName, $field->getSelectOptions(), $field->getValue(), $field->getAttributes()) !!}
+    @if($field->getHelp())
+        <p class="help-text">
+            {{$field->getHelp()}}
+        </p>
+    @endif
     <div class="text-red">
         {{ join($errors->get($fieldName), '<br />') }}
     </div>
