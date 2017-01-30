d6c01b3bd5fee907526b4c601b44a4645eddfa30
Ludmil Simeonov <lsimeonov@despark.com>
2017-01-30 14:26:36 +0200

    Add help text for date time fields

--- resources/views/admin/formElements/date.blade.php
+++ resources/views/admin/formElements/date.blade.php
@@ -8,6 +8,11 @@ $elementName = $field->getElementName();
     <div class="datepicker input-group">
         {!! Form::text($elementName, $field->getValue(), $field->getAttributes()) !!}
     </div>
+    @if($field->getHelp())
+        <p class="help-text">
+            {{$field->getHelp()}}
+        </p>
+    @endif
     <div class="text-red">
         {{ join($errors->get($fieldName), '<br />') }}
     </div>
