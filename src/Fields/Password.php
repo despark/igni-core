eaabb0e05ca56730432d3d333b18e97f23c002e6
Ludmil Simeonov <lsimeonov@despark.com>
2017-03-14 09:28:23 +0200

    Set autocomplete off for password field

--- src/Fields/Password.php
+++ src/Fields/Password.php
@@ -2,6 +2,24 @@
 
 namespace Despark\Cms\Fields;
 
+/**
+ * Class Password.
+ */
 class Password extends Text
 {
+
+    /**
+     * Password constructor.
+     * @param string $fieldName
+     * @param array  $options
+     * @param null   $value
+     */
+    public function __construct($fieldName, array $options, $value = null)
+    {
+        if (! isset($options['attributes'], $options['attributes']['autocomplete'])) {
+            $options['attributes']['autocomplete'] = 'off';
+        }
+        parent::__construct($fieldName, $options, $value);
+    }
+
 }
